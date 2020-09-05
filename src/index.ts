2a270e02098368a5b81da10358dc9bdffbd9a207
Xmader <xmader@outlook.com>
2020-09-05 01:31:42 -0400

    feat: index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+export * from "./post"
+export * from "./discussions"
+export * from "./topic"
+export * from "./user"
+
+export * from "./series"
+export { saveSeriesOf as default } from "./series"
